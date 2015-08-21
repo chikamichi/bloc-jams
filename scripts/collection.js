951fa31f38f23f4da3430b2e1338180f914f5506
kusera <kuserasera@gmail.com>
2015-08-21 01:45:45 -0400

    Refactor collection page with jQuery

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,24 +1,29 @@
-var collectionItemTemplate = 
+var buildCollectionItemTemplate = function () {
+	var template =
 	'<div class="collection-album-container column fourth">'
 +	'	<img src="assets/images/album_covers/01.png">'
-+	'			<div class="collection-album-info caption">'
-+	'				<p>'
-+	'					<a class="album-name" href="#">The Colors</a>'
-+	'					<br>'
-+	'					<a href="#">Pablo Picasso</a>'
-+	'					<br>'
-+	'					X songs'
-+	'					<br>'
-+	'				</p>'
-+	'			</div>'
-	'		</div>';
++	'	<div class="collection-album-info caption">'
++	'		<p>'
++	'			<a class="album-name" href="#">The Colors</a>'
++	'			<br>'
++	'			<a href="#">Pablo Picasso</a>'
++	'			<br>'
++	'			X songs'
++	'			<br>'
++	'		</p>'
++	'	</div>'
++	'</div>';
+	return $(template);
+};
 
-window.onload = function() {
-	var collectionContainer = document.getElementsByClassName("album-covers")[0];
+$(window).load(function() {
+	var $collectionContainer = $(".album-covers.clearfix");
+	$collectionContainer.empty();
 	
-	collectionContainer.innerHTML = "";
-	
+	console.log($collectionContainer.html());
+	console.log($collectionContainer);
 	for (var i = 0; i < 12; i++){
-		collectionContainer.innerHTML += collectionItemTemplate;
+		var $newThumbnail = buildCollectionItemTemplate();
+		$collectionContainer.append($newThumbnail);
 	}
-}
+});
